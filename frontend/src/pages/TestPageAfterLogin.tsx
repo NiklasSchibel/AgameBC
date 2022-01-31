366ee28ecb70e356459e34ec34a47ddd1e7a69b8
Niklas Schibel <niklas.schibel@gmx.de>
2022-01-31 16:09:41 +0100

    setup TestPage and Routes for Authentication Requirement

---
+++ frontend/src/pages/TestPageAfterLogin.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css';
+
+export default function TestPageAfterLogin () {
+    return (
+        <div className="App">
+            <header className="App-header">
+                TestHeader
+            </header>
+            <div>testpage for Login</div>
+        </div>
+    )
+}
