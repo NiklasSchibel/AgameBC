fc05b154cf9a6b15efba72241289993498492740
Niklas Schibel <niklas.schibel@gmx.de>
2022-01-31 16:50:33 +0100

    configure BrowserRouting for Authentication, working in Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import './App.css';
 import AuthProvider from "./context/AuthProvider";
 import LoginPage from "./pages/LoginPage";
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import RequireAuth from "./context/RequireAuth";
 import TestPageAfterLogin from "./pages/TestPageAfterLogin";
 
 export default function App() {
     return (
         <AuthProvider>
-            <Routes>
-                <Route path="*" element={<LoginPage/>}/>
-                <Route path="/Login" element={<LoginPage/>}/>
-                <Route path="/TestPage" element={<RequireAuth>
-                    <TestPageAfterLogin/>
-                </RequireAuth>}
-                />
-            </Routes>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="*" element={<LoginPage
+                    />}/>
+                    <Route path="/Login" element={<LoginPage
+                    />}/>
+                    <Route path="/TestPage" element={<RequireAuth>
+                        <TestPageAfterLogin/>
+                    </RequireAuth>}
+                    />
+                </Routes>
+            </BrowserRouter>
         </AuthProvider>
     );
 }
