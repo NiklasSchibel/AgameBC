d5618793c2eb3f8b2a0ae4dbbc7977f4e3af4095
Niklas Schibel <niklas.schibel@gmx.de>
2022-02-03 08:36:42 +0100

    PlayButton - functionality is working, Text-To-Speech

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -13,13 +13,19 @@ interface cardProps {
 export default function Card(props: cardProps) {
     const [text, setText] = useState<string>('');
     const language: string = "de-de";
-    //todo set key later in environment
+    //todo: set key later in environment
     const key: string = "a7aae25de0b446c7adc2571316a7ddfc&";
+
+    //todo: this works for the first time clicking on the picture than only clicking on the play button
+    const onClickHandle = () => {
+        setText(srcString);
+    }
+
+    //todo: images load to slow
 
     const srcString: string = "https://api.voicerss.org/?key="+key+"hl="+language+"&src=" + props.latin_name;
     return (
-        <div className="card">
-            {/*<h3>{props.name}</h3>*/}
+        <div onClick={onClickHandle} className="card">
             <img className="image" src={props.image_link} alt="Ein Bild"/>
             <React.Fragment>
                 <h4>{props.latin_name}</h4>
