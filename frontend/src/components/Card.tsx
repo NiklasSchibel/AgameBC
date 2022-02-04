c7a8f97811fe513dced5a395dda11bed3d23b4b4
Niklas Schibel <niklas.schibel@gmx.de>
2022-02-04 14:07:35 +0100

    getting random letters does work

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -14,14 +14,16 @@ interface cardProps {
 export default function Card(props: cardProps) {
     const {animal_type, image_link} = props
 
-    const ABCD = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     const LANGUAGE: string = "de-de";
-    const STANDARDTEXTVOICE: string = "mit welchem Buchstaben beginnt mein Name, ich heiße";
+    const STANDARDTEXTVOICE: string = "Ich heiße ";
     const [text, setText] = useState<string>('');
-    const firstLetterOfAnimalName = getFirstLetter(animal_type);
     const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const firstRandomLetter = generateNewRandomLetter(ALPHABET,firstLetterOfAnimalName);
-    const secondRandomLetter = generateNewRandomLetter(ALPHABET,firstLetterOfAnimalName,firstRandomLetter);
+    const firstLetterOfAnimalName = getFirstLetter(animal_type);
+    const firstRandomLetter = generateNewRandomLetter(ALPHABET, firstLetterOfAnimalName);
+    const secondRandomLetter = generateNewRandomLetter(ALPHABET, firstLetterOfAnimalName, firstRandomLetter);
+    // const firstButtonField =
+    // const secondButtonField =
+    // const thirdButtonField =
 
     //todo: set key later in environment
     const key: string = "a7aae25de0b446c7adc2571316a7ddfc&";
@@ -44,7 +46,7 @@ export default function Card(props: cardProps) {
      * */
     function getFirstLetter(word: string): string {
         if (word !== undefined && word.length > 2) {
-            return word.slice(0,1);
+            return word.slice(0, 1);
         } else {
             return word;
         }
@@ -58,12 +60,14 @@ export default function Card(props: cardProps) {
     function generateNewRandomLetter(alphabet: string, letter: string, secondLetter?: string) {
         const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
         while (randomLetter === letter || randomLetter === secondLetter) {
-            const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-            if (randomLetter === letter || randomLetter === secondLetter) {
-                return randomLetter;
+            const nextRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+            if (nextRandomLetter !== letter && nextRandomLetter !== secondLetter) {
+                return nextRandomLetter;
             }
         }
+        return randomLetter;
     }
+
 
 
     //todo: this works for the first time clicking on the picture than only clicking on the play button
@@ -84,9 +88,9 @@ export default function Card(props: cardProps) {
             </React.Fragment>
 
             <div className="ButtonsSelection">
-                <Button className="ButtonText" variant="outlined">{firstRandomLetter}</Button>
-                <Button className="ButtonText" variant="contained" color="success">{firstLetterOfAnimalName}</Button>
-                <Button className="ButtonText" variant="text" color="success">{secondRandomLetter}</Button>
+                <Button className="ButtonText" variant="outlined" color="success">{firstLetterOfAnimalName}</Button>
+                <Button className="ButtonText" variant="outlined" color="success">{firstRandomLetter}</Button>
+                <Button className="ButtonText" variant="outlined" color="success">{secondRandomLetter}</Button>
             </div>
         </div>
     )
