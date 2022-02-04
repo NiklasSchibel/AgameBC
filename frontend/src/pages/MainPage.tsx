35f98365450e19a64dc112bc029706c901453c5c
Niklas Schibel <niklas.schibel@gmx.de>
2022-02-04 08:57:42 +0100

    smaller changes to layout before pull request

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -4,7 +4,7 @@ import Gallery from "../components/Gallery";
 export default function MainPage(){
     return(
         <div className="mainPage">
-            <Header name="Animal Zoo API"/>
+            {/*<Header name="Animal Zoo API"/>*/}
             <Gallery/>
         </div>
     )
