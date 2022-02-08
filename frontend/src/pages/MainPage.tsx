5fe4a8355c219912156e6fa43f31e26f7dbf242e
Niklas Schibel <niklas.schibel@gmx.de>
2022-02-08 21:16:40 +0100

    MainPage Level3 Page added

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,6 +2,7 @@ import Level2Page from "./Level2Page";
 import Level1Page from "./Level1Page";
 import {useContext, useEffect, useState} from "react";
 import {AuthContext} from "../context/AuthProvider";
+import Level3Page from "./Level3Page";
 
 export default function MainPage(){
 
@@ -15,11 +16,15 @@ export default function MainPage(){
     if(level === undefined){
         setNewLevel(1)
         return<div>reload Page please</div>
-    } else if(level < 4) {
+    } else if(level < 2) {
         return <Level1Page
         rightAnswer={rightAnswer}
         setRightAnswer={setRightAnswer}
         />
-    } else return <Level2Page/>
+    } else if(level<3) {
+        return <Level2Page/>
+    }
+        else {
+        return <Level3Page/>}
 
 }
