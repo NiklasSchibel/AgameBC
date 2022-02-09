42149c81e98f928cc128c8913931235cab28602a
Niklas Schibel <niklas.schibel@gmx.de>
2022-02-09 11:28:22 +0100

    problem with key in Reorder.Item component

--- frontend/src/components/DragableItems.tsx
+++ frontend/src/components/DragableItems.tsx
@@ -1,33 +1,73 @@
-import { Reorder } from "framer-motion"
-import {Dispatch, useState} from "react";
+import {Reorder} from "framer-motion"
+import {Dispatch, useEffect, useState} from "react";
 
-interface DragableItemsProps{
+interface DragableItemsProps {
     key: string
     animalName: string
     id: string
     imageLink: string
     rightAnswer: string
-    setRightAnswer:  Dispatch<React.SetStateAction<string>>
+    setRightAnswer: Dispatch<React.SetStateAction<string>>
 }
 
-export default function DragableItems(props:DragableItemsProps) {
+export default function DragableItems(props: DragableItemsProps) {
     const [items, setItems] = useState([0, 1, 2, 3])
-    const letterArray = Array.of(props.animalName)
-    const letterArrayMap = new Map(JSON.parse(JSON.stringify([...letterArray])));
+    const letterString: String = new String(props.animalName); // the constructor leads to a warning but no work around found yet
+    // const letterstring:string = props.animalName; // does not work because of "can not read properties of undefined"
+    const letterArray = letterString.split(''); //this is an string string[]
+    const [letters, setLetters] = useState(letterArray)
+    const [choicesShuffled, setChoicesShuffled] = useState<Array<string>>([]);
+    const [answerTrue, setAnswerTrue] = useState<boolean>(false);
 
-    console.log("letterArray:",letterArray)
-    // console.log("LetterArrayMap", letterArrayMap)
+    useEffect(() => {
+        setChoicesShuffled(shuffleArray(letterArray))
+    }, [])
+
+    useEffect(() => {
+        console.log(checkIfArraysAreTheSame(choicesShuffled,letterArray))
+        console.log(letters)
+        console.log(choicesShuffled)
+        console.log(letterArray)
+    }, [choicesShuffled])
+
+    /**
+     * shuffle Function from W3 Schools
+     * @param array of type string
+     * returns the array in shuffeld order
+     * */
+    function shuffleArray(array: string[]) {
+        let curId = array.length;
+        while (0 !== curId) {
+            let randId = Math.floor(Math.random() * curId);
+            curId -= 1;
+            let tmp = array[curId];
+            array[curId] = array[randId];
+            array[randId] = tmp;
+        }
+        return array;
+    }
+
+    function checkIfArraysAreTheSame(stringArray1: string[], stringArray2: string[]): boolean {
+        return (JSON.stringify(stringArray1) === JSON.stringify(stringArray2))
+    }
 
     return (
         <div>
             <h4>{props.animalName}</h4>
-        <Reorder.Group axis="y" values={items} onReorder={setItems}>
-            {items.map(item => (
-                <Reorder.Item key={item} value={item}>
-                    {item}
-                </Reorder.Item>
-            ))}
-        </Reorder.Group>
+            <Reorder.Group as="ol" axis="y" values={choicesShuffled} onReorder={setChoicesShuffled}>
+                {choicesShuffled.map(letter => (
+                    <Reorder.Item key={letter} value={letter}>
+                        {letter}
+                    </Reorder.Item>
+                ))}
+            </Reorder.Group>
+            <Reorder.Group axis="y" values={items} onReorder={setItems}>
+                {items.map(item => (
+                    <Reorder.Item key={item} value={item}>
+                        {item}
+                    </Reorder.Item>
+                ))}
+            </Reorder.Group>
         </div>
-            )
+    )
 }
