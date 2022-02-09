376a225ae92c7b8b1cff5d07b2fa2fad4b47fc6d
Niklas Schibel <niklas.schibel@gmx.de>
2022-02-09 14:14:17 +0100

    smile for true answer and level() function added to Level3

--- frontend/src/components/DragableItems.tsx
+++ frontend/src/components/DragableItems.tsx
@@ -1,6 +1,7 @@
 import {Reorder} from "framer-motion"
-import {Dispatch, useContext, useEffect, useState} from "react";
+import React, {Dispatch, useContext, useEffect, useState} from "react";
 import {LevelContext} from "../context/LevelProvider";
+import smile from "../images/iconSmile.png";
 
 interface DragableItemsProps {
     key: string
@@ -10,25 +11,32 @@ interface DragableItemsProps {
 }
 
 export default function DragableItems(props: DragableItemsProps) {
+    const [answer, setAnswer] = useState<boolean>(false);
     const letterString: String = new String(props.animalName); // the constructor leads to a warning but no work around found yet
     // const letterstring:string = props.animalName; // does not work because of "can not read properties of undefined"
     const letterArray = letterString.split(''); //this is an string[]
     // const [letters, setLetters] = useState(letterArray)
     const [choicesShuffled, setChoicesShuffled] = useState<Array<string>>([]);
     // const [answerTrue, setAnswerTrue] = useState<boolean>(false);
-    const {level,setNewLevel} = useContext(LevelContext)
+    const {level, setNewLevel} = useContext(LevelContext)
 
     useEffect(() => {
         setChoicesShuffled(shuffleArray(letterArray))
     }, [])
 
 
-// put this in onChange prop of renderComponten
+// put this in onChange prop of renderComponten does not work
     useEffect(() => {
-        checkIfArraysAreTheSame(choicesShuffled, letterArray)
-        // console.log(letters)
-        // console.log(choicesShuffled)
-        // console.log(letterArray)
+        if (checkIfArraysAreTheSame(choicesShuffled, letterArray)) {
+            setAnswer(true)
+            setTimeout(function(){
+                if (level === undefined) {
+                    setNewLevel(1)
+                } else {
+                    setNewLevel(level + 1)
+                }
+            }, 2000);
+        }
     }, [choicesShuffled])
 
     /**
@@ -52,21 +60,29 @@ export default function DragableItems(props: DragableItemsProps) {
         return (JSON.stringify(stringArray1) === JSON.stringify(stringArray2))
     }
 
-    function handleChange(){
-        console.log(choicesShuffled)
-        console.log("handleChange run")
+
+    /**
+     * this function returns a smile when
+     * @param givenAnswer is true
+     */
+    const AnswerTrueComponent = () => {
+        return (
+            <div>
+                <img className="SmileImage" src={smile} alt = "smile"/>
+            </div>)
     }
 
     return (
         <div>
             <h4>{props.animalName}</h4>
-            <Reorder.Group as="ol" axis="y" values={choicesShuffled} onChange={handleChange} onReorder={setChoicesShuffled}>
+            <Reorder.Group as="ol" axis="y" values={choicesShuffled} onReorder={setChoicesShuffled}>
                 {choicesShuffled.map((item, index) => (
-                    <Reorder.Item key={item+index} value={item}>
+                    <Reorder.Item key={item + index} value={item}>
                         {item}
                     </Reorder.Item>
                 ))}
             </Reorder.Group>
+            {answer && <AnswerTrueComponent/>}
         </div>
     )
 }
