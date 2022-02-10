0a0fdf75084b241f96141976b94f36a088e50aab
Niklas Schibel <niklas.schibel@gmx.de>
2022-02-10 14:40:04 +0100

    AuthProvider reduced/cleaned

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -5,15 +5,10 @@ export interface AuthContextType {
     token?: string
     jwtDecoded?: { sub?: string, exp?: number }
     setJwt: (jwt: string) => void
-    level?: number
-    setNewLevel: (newLevel: number) => void
 }
 
 export const AuthContext = createContext<AuthContextType>({
     setJwt: (data) => {
-        throw Error("defaut function has not been initialized")
-    },
-    setNewLevel: () => {
         throw Error("defaut function has not been initialized")
     }
 })
@@ -22,19 +17,15 @@ export default function AuthProvider({children}: { children: ReactElement<any, a
 
     const [token, setToken] = useState<string>()
     const [jwtDecoded, setJwtDecoded] = useState({})
-    const [level, setLevel] = useState<number>(1)
 
     const setJwt = (jwt: string) => {
         setToken(jwt)
         setJwtDecoded(jwt_decode(jwt.toString()))
     }
 
-    const setNewLevel = (newLevel: number) => {
-        setLevel(newLevel)
-    }
 
     return (
-        <AuthContext.Provider value={{token, jwtDecoded, setJwt, level, setNewLevel}}>
+        <AuthContext.Provider value={{token, jwtDecoded, setJwt}}>
             {children}
         </AuthContext.Provider>
     )
