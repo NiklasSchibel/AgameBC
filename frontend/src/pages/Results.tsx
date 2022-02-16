115a328eca0b47653936e06c01dff498714fee25
Niklas Schibel <niklas.schibel@gmx.de>
2022-02-16 15:21:17 +0100

    short layout

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -1,13 +1,13 @@
 import {getResult} from "../services/RequestService";
 import {useContext, useEffect, useState} from "react";
 import {AuthContext} from "../context/AuthProvider";
-import {ResultsData} from "../models/ResultsData";
+import './stylingPages/ResultsPage.scss';
 // @ts-ignore
 import {TagCloud} from 'react-tagcloud';
 import {LetterCount} from "../models/LetterCount";
 
 export default function Results() {
-    const {token, jwtDecoded} = useContext(AuthContext)
+    const {token} = useContext(AuthContext)
     const [results, setResults] = useState<LetterCount>({});
 
 //todo: nur token mitschicken und im Backend über principal datenbankabfrage laufen lassen
@@ -16,7 +16,7 @@ export default function Results() {
             .then(result => {
                 console.log(result)
                 return result;
-            }) //setResults(result.lettersCount)
+            })
             .then(result => setResults(result.lettersCount))
             .catch(error => console.log(error))
         // eslint-disable-next-line
@@ -30,27 +30,44 @@ export default function Results() {
     //         kein Spieler angemeldet, deswegen können keine results angezeigt werden!
     //     </div>)
     // }
-    console.log("results:", results)
-    console.log(results.a)
+
 
     const data = [
-        {value: 'A', count: results.a},
-        // {value: 'B', count: results?.lettersCount.get("B")},
-        // {value: 'C', count: results?.lettersCount.get("C")},
-        // {value: 'D', count: results?.lettersCount.get("D")},
-        {value: 'E', count: 2},
-        {value: 'F', count: 3},
-        {value: 'G', count: 1},
+        {value: 'A', count: results.A},
+        {value: 'B', count: results.B},
+        {value: 'C', count: results.C},
+        {value: 'D', count: results.D},
+        {value: 'E', count: results.E},
+        {value: 'F', count: results.F},
+        {value: 'G', count: results.G},
+        {value: 'H', count: results.H},
+        {value: 'I', count: results.I},
+        {value: 'J', count: results.J},
+        {value: 'K', count: results.K},
+        {value: 'L', count: results.L},
+        {value: 'M', count: results.M},
+        {value: 'N', count: results.N},
+        {value: 'O', count: results.O},
+        {value: 'P', count: results.P},
+        {value: 'Q', count: results.Q},
+        {value: 'R', count: results.R},
+        {value: 'S', count: results.S},
+        {value: 'T', count: results.T},
+        {value: 'U', count: results.U},
+        {value: 'V', count: results.V},
+        {value: 'W', count: results.W},
+        {value: 'X', count: results.X},
+        {value: 'Y', count: results.Y},
+        {value: 'Z', count: results.Z},
     ]
-    // @ts-ignore
-    return (<div>
-        <TagCloud
-            minSize={10}
-            maxSize={120}
-            tags={data}
-            onClick={(tag: { value: any; }) => alert(`'${tag.value}' was selected!`)}
-        />
-        {/*<div>{results?.get("A")}</div>*/}
-        {/*<div>{results.get("A")}</div>*/}
-    </div>)
+
+    return (
+        <div className="resultsPage">
+            <TagCloud className="tagCloud"
+                minSize={10}
+                maxSize={120}
+                tags={data}
+                onClick={(tag: { value: any; }) => alert(`'${tag.value}' was selected!`)}
+            />
+        </div>)
 }
